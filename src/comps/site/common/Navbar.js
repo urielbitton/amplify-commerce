1970e0ad9a2bfa62aab57d300dff9a728081fc8b
Uriel Bitton <55159607+urielbitton@users.noreply.github.com>
2021-05-06 00:36:14 -0400

    Logo design

--- src/comps/site/common/Navbar.js
+++ src/comps/site/common/Navbar.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import './styles/Navbar.css'
 
 export default function Navbar() {
   return (
-    <nav className="navbar">
-
-    </nav>
+    <>
+      <nav className="navbar">
+        <div className="left">
+          <div className="logocont">
+            <img src="https://i.imgur.com/43BCbLO.png" alt=""/>
+            <h5>Amplify<span>.</span></h5>
+          </div>
+        </div>
+        <div className="right">
+          
+        </div>
+      </nav>
+      <div style={{height:'80px'}}></div>
+    </>
   )
 }
 
