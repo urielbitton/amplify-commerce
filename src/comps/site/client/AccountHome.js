89b15e399c4b70681638e2a1d619cb0c34c55274
Uriel Bitton <55159607+urielbitton@users.noreply.github.com>
2021-05-23 18:33:21 -0400

    home ui

--- src/comps/site/client/AccountHome.js
+++ src/comps/site/client/AccountHome.js
@@ -1,12 +1,19 @@
 import React from 'react'
+import './styles/AccountHome.css'
 
 export default function AccountHome()  {
   return (
     <div className="accounthomepage">
-      Account Home
-      ...
-      <br/>
-      Find an order (enter order number to find an order)
+      {/*Find an order (enter order number to find an order)*/}
+      <div className="homebox">
+        <h4>Recent Orders</h4>
+      </div>
+      <div className="homebox">
+        <h4>Track Your Orders</h4>
+      </div>
+      <div className="homebox full">
+        <h4>Recent Purchases</h4>
+      </div>
     </div>
   )
 }
