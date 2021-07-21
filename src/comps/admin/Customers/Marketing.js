482a9474cc7333e87d0000badad95e04b37cead5
Uriel Bitton <55159607+urielbitton@users.noreply.github.com>
2021-07-21 01:25:42 -0400

    init reviews page

---
+++ src/comps/admin/Customers/Marketing.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Marketing() {
+
+  return (
+    <div className="marketingpage">
+      Allow admin to send marketing email (using SendMail) to all customers emails
+      Allow also option to create ads on site - create marketing ad feature
+    </div>
+  )
+}
--- src/comps/admin/Customers/Reviews.js
+++ src/comps/admin/Customers/Reviews.js
@@ -5,7 +5,8 @@ export default function Reviews() {
   return (
     <div className="reviewspage">
       <div className="pagecont">
-        Customer Reviews
+        Table of all reviews (allow moderating reviews: delete only - no creating, editing)
+        OneCustomer page will show all reviews of a customer as ReviewCard component
       </div>
     </div>
   )
