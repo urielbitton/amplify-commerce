0b25153e98a74583e04c1632724c49e37a93a0c3
Uriel Bitton <55159607+urielbitton@users.noreply.github.com>
2021-08-10 00:54:52 -0400

    added error boundaries

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import "./styles.css"
 import { BrowserRouter as Router } from "react-router-dom"
 import StoreContextProvider from "./comps/common/StoreContext"
 import AppContainer from './comps/site/containers/AppContainer'
+import ErrorBoundary from './ErrorBoundary'
 
 export default function App() { 
   return (
     <div className="App">
       <StoreContextProvider>
-        <Router>
-          <AppContainer />
-        </Router>
+        <ErrorBoundary>
+          <Router>
+            <AppContainer />
+          </Router>
+        </ErrorBoundary>
       </StoreContextProvider>
     </div>
   );
---
+++ src/ErrorBoundary.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+class ErrorBoundary extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+  
+
+  static getDerivedStateFromError(error) {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+  componentDidCatch(error, errorInfo) {
+    // You can also log the error to an error reporting service
+    //send email to admin with error log
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div className="errorpage">
+          <img src="https://i.imgur.com/z8nfahP.png" alt=""/>
+          <h1>An error occured on this page</h1>
+          <h6>Don't worry we're working on fixing this bug so it doesn't happen again.</h6>
+          <a href="https://6hui4.csb.app.com"><button>Back Home</button></a>
+        </div>
+      )
+    }
+    return this.props.children; 
+  }
+}
+
+export default ErrorBoundary
